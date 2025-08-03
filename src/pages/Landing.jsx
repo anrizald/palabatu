8df7c4a529f7710ec65c4c881b1759ed14b68fc6
ghul <anrizald@gmail.com>
2025-08-03 16:59:09 +0700

    pages and leaflet integration

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,32 @@
+export default function Landing() {
+    return (
+        <div className="h-screen w-full snap-y snap-mandatory overflow-y-scroll scroll-smooth ">
+            {/* Hero Section */}
+            <section className="h-screen w-full snap-start flex flex-col justify-center items-center bg-gradient-to-b from-green-100 to-white text-center">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">palabatu</h1>
+                <p className="text-lg md:text-xl mb-8 text-black">Lorem Ipsum</p>
+                <a
+                    href="/map"
+                    className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+                > Open Map </a>
+            </section>
+
+            {/* About Section */}
+            <section className="h-screen w-full snap-start flex flex-col justify-center items-center bg-gradient-to-b from-green-100 to-white text-center">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">about palabatu</h1>
+                <p className="text-lg md:text-xl mb-8 text-black">Lorem Ipsum</p>
+            </section>
+
+            {/* Explore Preview Section */}
+            <section className="min-h-screen w-full snap-start flex flex-col justify-center items-center bg-gradient-to-b from-green-100 to-white text-center">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 text-black">palabatu</h1>
+                <p className="text-lg md:text-xl mb-8">Lorem Ipsum</p>
+
+                {/* Footer Section */}
+                <footer className="py-6 text-center text-sm bg-gray-100 text-gray-500 items-end">
+                    © {new Date().getFullYear()} palabatu — WC Ass Production.
+                </footer>
+            </section>
+        </div>
+    )
+}
